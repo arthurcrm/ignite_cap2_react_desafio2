30f9988e12059347872f21d1f6689671ae310ba8
Arthur Menezes <arthur.menezes@presidencia.gov.br>
2021-06-28 18:08:19 -0300

    Add: Refactor ModalAddFood Component

---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,44 @@
+import { Component, createRef, useRef } from "react";
+import { FiCheckSquare } from "react-icons/fi";
+
+import { Form } from "./styles";
+import { Modal } from "../Modal";
+import { Input } from "../Input";
+
+interface ModalAddFoodProps {
+  setIsOpen: () => void;
+  handleAddFood: (data: {}) => void;
+  isOpen: boolean;
+}
+
+export const ModalAddFood = ({
+  isOpen,
+  handleAddFood,
+  setIsOpen,
+}: ModalAddFoodProps) => {
+  const handleSubmit = async (data: {}) => {
+    handleAddFood(data);
+    setIsOpen();
+  };
+  const formRef = useRef(null);
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
